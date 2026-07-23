31af26ff3a1bdbf9be669c7b6cbfeb5e6640809f
Tabolrin <93069446+Tabolrin@users.noreply.github.com>
2026-07-24 02:55:57 +0300

    Used CompareTag instead of a string check, and stopped looking up the same component twice when the player gets hit

--- Assets/Space Shooter Template FREE/Scripts/Enemy.cs
+++ Assets/Space Shooter Template FREE/Scripts/Enemy.cs
@@ -62,10 +62,11 @@ public class Enemy : MonoBehaviour {
     //if 'Enemy' collides 'Player', 'Player' gets the damage equal to projectile's damage value
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.CompareTag("Player"))
         {
-            if (Projectile.GetComponent<Projectile>() != null)
-                Player.instance.GetDamage(Projectile.GetComponent<Projectile>().damage);
+            var projectile = Projectile.GetComponent<Projectile>();
+            if (projectile != null)
+                Player.instance.GetDamage(projectile.damage);
             else
                 Player.instance.GetDamage(1);
         }
